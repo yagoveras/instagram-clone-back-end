0d1f5722b89ad035ae2963e89ecf00f294c2d2a2
unknown <yg.lima@outlook.com>
2022-04-07 00:10:24 -0300

    implementação do token com jwt

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,22 +1,36 @@
 import type { NextApiRequest, NextApiResponse  } from 'next'
 import { conectarMongoDB } from '../../middlewares/conectarMongodb'
 import type { RespostaPadraoMsg } from '../../types/respostaPadrao'
+import type { loginResposta } from '../../types/loginResposta'
 import md5 from 'md5';
 import { UsuarioModel } from '../../models/UsuarioModel';
+import jwt from 'jsonwebtoken';
+
+
 const endPointLogin = async (
     req: NextApiRequest,
-    res: NextApiResponse< RespostaPadraoMsg > )=>{
+    res: NextApiResponse< RespostaPadraoMsg >| any)=>{
+
+        const {MINHA_CHAVE_JWT} = process.env;
+        if(!MINHA_CHAVE_JWT){
+            res.status(500).json({erro: 'ENV JWT não informada'})
+        }
     if(req.method === 'POST'){
         const {login, senha} = req.body
         
         const usuarioEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)})
         if(usuarioEncontrados && usuarioEncontrados.length > 0){
             const usuarioEncontrado = usuarioEncontrados[0];
-                return res.status(200).json({msg: `Usuario ${usuarioEncontrado.nome} autenticado com sucesso`})
+                const token = jwt.sign({_id : usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+                return res.status(200).json({
+                    nome: usuarioEncontrado.nome,
+                    email : usuarioEncontrado.email,
+                    token});
         
             }
             return res.status(400).json({erro: 'Usuario ou senha não encontrado'})
     }
+    
     return res.status(405).json({
         erro: 'Metodo Informado não é valido'
     })
